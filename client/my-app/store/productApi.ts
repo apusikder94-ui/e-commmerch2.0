ff0254444901a3cb0f2184a761ae7653a5e275c2
Apu Sikder <apusikder750@gmail.com>
2026-06-25 18:54:15 +0600

    add proct api

--- client/my-app/store/productApi.ts
+++ client/my-app/store/productApi.ts
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${BASE_URL}/product`,
+    baseUrl: `${BASE_URL}/api/v1/product`,
   }),
   tagTypes: ["Product"],
 
